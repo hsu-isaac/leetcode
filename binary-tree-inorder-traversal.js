5666355960722fb23a3e0e33352ae01f5b30797d
Isaac Hsu <16hsuisaac@gmail.com>
2023-04-17 13:02:15 -0700

    solved binary tree inorder traversal with an interative loop

---
+++ binary-tree-inorder-traversal.js
@@ -0,0 +1,35 @@
+/*
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+*/
+
+var inorderTraversal = function (root) {
+  const stack = [];
+  const res = [];
+
+  while (root || stack.length) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else {
+      root = stack.pop();
+      res.push(root.val);
+      root = root.right;
+    }
+  }
+
+  return res;
+};
