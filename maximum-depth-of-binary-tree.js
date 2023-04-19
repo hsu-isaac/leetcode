d06233b121c7f6b7a959ee98843acc071c5937d3
Isaac Hsu <16hsuisaac@gmail.com>
2023-04-19 14:27:12 -0700

    solved maximum depth of binary tree ee using a recursive formula to add depth with each interation while going down the tree

---
+++ maximum-depth-of-binary-tree.js
@@ -0,0 +1,20 @@
+/*
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+*/
+
+var maxDepth = function (root) {
+  if (root === undefined || root === null) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
