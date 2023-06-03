54ad16aef40e5cc90a4dd40aa54871633695ac33
Isaac Hsu <16hsuisaac@gmail.com>
2023-06-02 22:46:49 -0700

    fixed typo in grou anagrams

--- group-anagrams.js
+++ group-anagrams.js
@@ -23,7 +23,7 @@ var groupAnagrams = function (strs) {
   let obj = {};
   for (let str of strs) {
     let letters = str.split("").sort().join("");
-    obj[letters] ? obj[letters].push(string) : obj[letters] = [str];
+    obj[letters] ? obj[letters].push(str) : obj[letters] = [str];
   }
   return Object.values(obj);
 };
