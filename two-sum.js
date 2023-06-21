7b17ee59fc9007e6895dda72da8d3492c7c6316b
Isaac Hsu <16hsuisaac@gmail.com>
2023-06-21 16:38:12 -0700

    recording second twosum attempt

--- two-sum.js
+++ two-sum.js
@@ -53,3 +53,16 @@ var twoSum = function(nums, target) {
   }
   return [];
 };
+
+
+var twoSum = function (nums, target) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[target - nums[i]] !== undefined) {
+      return [hash[target - nums[i]], i]
+    } else {
+      hash[nums[i]] = i
+    }
+  }
+  return [];
+};
