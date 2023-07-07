a7d94c43897afa5f0f644e3b4907a01bc2d582ef
Isaac Hsu <16hsuisaac@gmail.com>
2023-07-07 14:04:36 -0700

    solved add two numbers again from scscratch

--- add-two-numbers.js
+++ add-two-numbers.js
@@ -49,3 +49,31 @@ var addTwoNumbers = function (l1, l2) {
 
   return head.next;
 };
+
+
+var addTwoNumbers = function (l1, l2) {
+  let head = new ListNode(0);
+  let res = head;
+  let carry = 0;
+  let sum = 0;
+
+  while (l1 != null || l2 != null || sum > 0) {
+    if (l1 != null) {
+      sum = sum + l1.val;
+      l1 = l1.next;
+    } if (l2 != null) {
+      sum = sum + l2.val;
+      l2 = l2.next;
+    } if (sum >= 10) {
+      carry = 1;
+      sum = sum - 10;
+    }
+
+    res.next = new ListNode(sum);
+    sum = carry;
+    carry = 0;
+    res = res.next;
+  }
+
+  return head.next;
+};
